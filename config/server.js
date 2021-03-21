64303abeb9c76fb80631aa0207e09b253576342d
Samit Koyom <samitkoyom@gmail.com>
2021-03-21 22:37:52 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07dba122e9a416551ed4f880ece5ecad'),
+    },
+  },
+});
